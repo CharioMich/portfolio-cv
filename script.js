ab12381ef32e837e00ba7fef5df39e0b2fdcd6f6
Ch. Michalakis <mpampisb@gmail.com>
2025-05-22 14:30:33 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+/**
+ * Updates data-bs-dismiss attribute on offcanvas li elements 
+ * so the offcanvas can dissapear after section selection on smaller screens.
+ * Also toggles w-50 class on about me section cards
+ */ 
+function updateOffcanvasDismiss() {
+  const navLinks = document.querySelectorAll(".offcanvas-body .nav-item");
+  const aboutCards = document.querySelectorAll(".cf-about");
+
+  if (window.innerWidth < 768) {
+    navLinks.forEach((link) => {
+      link.setAttribute("data-bs-dismiss", "offcanvas");
+    });
+    aboutCards.forEach((card) => {
+      card.classList.remove("w-50");
+    });
+  } else {
+    navLinks.forEach((link) => {
+      link.removeAttribute("data-bs-dismiss");
+    });
+    aboutCards.forEach((card) => {
+      card.classList.add("w-50");
+    });
+  }
+}
+
+// Run on page load
+updateOffcanvasDismiss();
+
+// Update on window resize
+window.addEventListener("resize", updateOffcanvasDismiss);
